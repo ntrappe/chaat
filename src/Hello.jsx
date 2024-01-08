50369d04ff948491e8ec169120d3c9925ca8e6e4
Nicole Trappe <ntrappe@ucsd.edu>
2024-01-08 10:48:26 -0800

    finished styled component for lang selector! [GOOD]

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,14 +1,163 @@
-function Hello() {
+import React from 'react';
+import styled from 'styled-components';
 
+/* Custom Colors */
+const highlightBlue = '#388eff';
+const darkerGrey = '#1f1f1f';
+const mediumGrey = '#3d3d3d';
+const borderGrey = '#444';
+const lightGrey = '#999';
+
+const LanguageOption = styled.button`
+  font-size: 0.8em;
+  font-weight: 600;
+  border-radius: 5px;
+  padding: 4px 8px;
+  margin-bottom: 6px;
+  background-color: ${(props) => {
+    if (props.$state === "on") {
+      return highlightBlue;
+    } else {
+      return darkerGrey;
+    }
+  }};
+  color: ${(props) => {
+    if (props.$state === "on") {
+      return 'white';
+    } else {
+      return lightGrey;
+    }
+  }};
+
+  &:hover {
+    background-color: ${(props) => {
+      if (props.$state === 'on') {
+        return highlightBlue;
+      } else {
+        return mediumGrey;
+      }
+    }}
+  }
+`;
+
+const LanguageSelector = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 251px;
+  left: 3em;
+  padding: 12px 3.5px;
+  background-color: #1F1F1F;
+  border: 1px solid #444;
+  border-radius: 12px;
+
+  @media (max-width: 1068px) {
+    top: 192px;
+  }
+
+  @media (max-width: 767px) {
+    display: none;
+  }
+`;
+
+const FontOption = styled.button`
+  font-size: 1em;
+  font-weight: 600;
+  border-radius: 5px;
+  padding: 3.5px 6.5px;
+  margin-bottom: 6px;
+  background-color: ${(props) => (props.state === 'off' ? '#1f1f1f' : '#388eff')}
+  color: ${(props) => (props.state === 'off' ? '#999' : 'white')}
+  
+  &:hover {
+    background-color: ${(props) => (props.state === 'off' ? '#3d3d3d' : '388eff')}
+    color: ${(props) => (props.state === 'off' ? '#d3d3d3' : 'white')}
+  }
+  
+  &:first-child {
+    font-family: 'SF Compact';
+    font-weight: 600;
+  }
+  
+  &:nth-child(2) {
+    font-family: 'SF Mono';
+    font-weight: 200;
+  }
+  
+  &:last-child {
+    font-family: 'New York';
+    font-weight: 400;
+  }
+`;
+
+const FontSelector = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 310px;
+  right: 6.25em;
+  padding: 10px 3px;
+  background-color: #1F1F1F;
+  border: 1px solid #444;
+  border-radius: 12px;
+
+  @media (max-width: 1068px) {
+    top: 250px;
+  }
+
+  @media (max-width: 767px) {
+    display: none;
+  }
+`;
+
+function Hello() {
   return (
     <>
-      <figure id="hello-background"></figure>
-      <div id="hello-text-container">
-        <h1 id="hello-text" family="romance">Hello</h1>
-        <span id="text-cursor"></span>
-      </div>
+      <LanguageSelector>
+        <LanguageOption $state="off">AR</LanguageOption>
+        <LanguageOption $state="on">EN</LanguageOption>
+        <LanguageOption $state="off">ES</LanguageOption>
+        <LanguageOption $state="off">FR</LanguageOption>
+        <LanguageOption $state="off">HI</LanguageOption>
+        <LanguageOption $state="off">RU</LanguageOption>
+        <LanguageOption $state="off">ZN</LanguageOption>
+      </LanguageSelector>
+      {/* <FontSelector>
+        <FontOption state="off">Aa</FontOption>
+        <FontOption state="off">Aa</FontOption>
+        <FontOption state="on">Aa</FontOption>
+      </FontSelector> */}
     </>
   )
+  // return (
+  //   <>
+  //     <figure id="hello-background"></figure>
+  //     <div id="hello-text-container">
+  //       <h1 id="hello-text" family="romance">Hello</h1>
+  //       <span id="text-cursor"></span>
+  //     </div>
+  //     <div id="language-selector" className="hello-text-selector" state="visible">
+  //       <button className="lang-opt" state="off">AR</button>
+  //       <button className="lang-opt" state="on">EN</button>
+  //       <button className="lang-opt" state="off">ES</button>
+  //       <button className="lang-opt" state="off">FR</button>
+  //       <button className="lang-opt" state="off">HI</button>
+  //       <button className="lang-opt" state="off">RU</button>
+  //       <button className="lang-opt" state="off">ZN</button>
+  //     </div>
+  //     <div id="font-selector" className="hello-text-selector" state="visible">
+  //       <button className="font-opt" state="off">Aa</button>
+  //       <button className="font-opt" state="off">Aa</button>
+  //       <button className="font-opt" state="on">Aa</button>
+  //     </div>
+  //     <div id="color-selector" className="hello-text-selector" state="visible">
+  //       <button class="color-opt" state="off"></button>
+  //       <button class="color-opt" state="on"></button>
+  //       <button class="color-opt" state="off"></button>
+  //       <button class="color-opt" state="off"></button>
+  //     </div>
+  //   </>
+  // )
 }
   
 export default Hello;
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Hello from './Hello.jsx';
-import './Hello.css';
 import './assets/fonts/fonts.css';
 
 const root = document.getElementById('hello-section');
