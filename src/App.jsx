af36340c7baee6eb1167eedc2f224142779cd792
Nicole Trappe <ntrappe@ucsd.edu>
2024-01-21 22:40:27 -0800

    take 2 with router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import ProjectsHome from './pages/ProjectsHome.jsx';
 import Project from './pages/Project.jsx';
@@ -13,21 +13,21 @@ import Art from './pages/Art.jsx';
 import Career from './pages/Career.jsx';
 import PageNotFound from './pages/PageNotFound.jsx';
 
-const router = createBrowserRouter();
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}>
+export default function App() {
+  return (
+    <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/projects' element={<ProjectsHome />} />
         <Route path='/projects/pomodoro' element={<Project customComponent={Pomodoro} />} />
         <Route path='/projects/rock' element={<Project customComponent={Rock} />} />
-        <Route path='projects/bookify' element={<Project customComponent={Bookify} />} />
+        <Route path='/projects/bookify' element={<Project customComponent={Bookify} />} />
         <Route path='/photography' element={<Photography />} />
         <Route path='/career' element={<Career />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
-    </RouterProvider>
-  </React.StrictMode>,
-);
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
