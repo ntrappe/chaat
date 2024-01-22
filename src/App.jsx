42521a14a9a9c36bd55d2bb29a723a1ab92e5ecb
Nicole Trappe <ntrappe@ucsd.edu>
2024-01-21 22:31:19 -0800

    try new routes?

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Routes, Route } from 'react-router-dom';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import ProjectsHome from './pages/ProjectsHome.jsx';
 import Project from './pages/Project.jsx';
@@ -12,55 +13,21 @@ import Art from './pages/Art.jsx';
 import Career from './pages/Career.jsx';
 import PageNotFound from './pages/PageNotFound.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-    errorElement: <PageNotFound />
-  },
-  {
-    path: '/projects',
-    element: <ProjectsHome />,
-    errorElement: <PageNotFound />
-  },
-  {
-    path: '/projects/pomodoro',
-    element: <Project customComponent={Pomodoro} />,
-    errorElement: <PageNotFound />
-  },
-  {
-    path: '/projects/rock',
-    element: <Project customComponent={Rock} />,
-    errorElement: <PageNotFound />
-  },
-  {
-    path: '/projects/bookify',
-    element: <Project customComponent={Bookify} />,
-    errorElement: <PageNotFound />
-  },
-  {
-    path: '/photography',
-    element: <Photography />,
-    errorElement: <PageNotFound />
-  },
-  {
-    path: '/art',
-    element: <Art />,
-    errorElement: <PageNotFound />
-  },
-  {
-    path: '/career',
-    element: <Career />,
-    errorElement: <PageNotFound />
-  },
-  {
-    path: '*',
-    element: <PageNotFound />
-  },
-]);
+const router = createBrowserRouter();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<ProjectsHome />} />
+        <Route path='/projects/pomodoro' element={<Project customComponent={Pomodoro} />} />
+        <Route path='/projects/rock' element={<Project customComponent={Rock} />} />
+        <Route path='projects/bookify' element={<Project customComponent={Bookify} />} />
+        <Route path='/photography' element={<Photography />} />
+        <Route path='/career' element={<Career />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </RouterProvider>
   </React.StrictMode>,
 );
