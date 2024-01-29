3e5f10baea584680a43200830ab0c4ec119378ca
Nicole Trappe <ntrappe@ucsd.edu>
2024-01-28 17:43:10 -0800

    fix err

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Routes, Route, HashRouter } from 'react-router-dom';
-import ProjectsHome from './pages/ProjectsHome.jsx';
-import Project from './pages/Project.jsx';
 import Bookify from './cases/Bookify.jsx';
 import Pomodoro from './cases/Pomodoro.jsx';
 import Rock from './cases/Rock.jsx';
@@ -11,16 +9,18 @@ import Photography from './pages/Photography.jsx';
 import Art from './pages/Art.jsx';
 import Career from './pages/Career.jsx';
 import PageNotFound from './pages/PageNotFound.jsx';
+import Page from './components/Core/Page.jsx';
+import ProjectGrid from './components/Core/ProjectGrid.jsx';
 
 export default function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/projects' element={<ProjectsHome />} />
-        <Route path='/projects/pomodoro' element={<Project customComponent={Pomodoro} />} />
-        <Route path='/projects/rock' element={<Project customComponent={Rock} />} />
-        <Route path='/projects/bookify' element={<Project customComponent={Bookify} />} />
+        <Route path='/projects' element={<Page customComponent={ProjectGrid} showAside={false} />} />
+        <Route path='/projects/pomodoro' element={<Page customComponent={Pomodoro} showAside={true} />} />
+        <Route path='/projects/rock' element={<Page customComponent={Rock} showAside={true} />} />
+        <Route path='/projects/bookify' element={<Page customComponent={Bookify} showAside={true} />} />
         <Route path='/photography' element={<Photography />} />
         <Route path='/career' element={<Career />} />
         <Route path='/art' element={<Art />} />
