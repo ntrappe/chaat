62e184051fc86dc7832db2a7f481d9612ad2ac8b
Nicole Trappe <ntrappe@ucsd.edu>
2024-02-03 15:08:45 -0800

    added video demo and transcript

--- src/cases/Pomodoro.jsx
+++ src/cases/Pomodoro.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import styled from 'styled-components';
 import PomodoroPrev from '../assets/case-study-images/pomodoro/pomodoro-preview.png';
 import Pomo1 from '../assets/case-study-images/pomodoro/pomo1.png';
@@ -184,7 +184,131 @@ const OverviewBox = styled.div`
   }
 `;
 
+const DynamicButtonCont = styled.div`
+  display: flex;
+  border-bottom: 1.25px solid var(--cloud);
+`;
+
+const DynamicButton = styled.button`
+  padding: 1.5rem 0.8rem 0.8rem 0;
+  font-family: 'SF Pro Display';
+  font-weight: ${(props) => (props.selected ? '500' : '300')};
+  color: ${(props) => (props.selected ? `var(--pavement)` : `var(--concrete)`)};
+  font-size: 0.85em;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const DynamicVideo = styled.div`
+  position: relative; /* Establishes positioning context for absolute children */
+  width: 100%;
+  min-height: 200px;
+`;
+
+const Video = styled.div`
+  position: absolute;   /* relative to parent */
+  top: 0;
+  left: 0;
+  width: 100%;
+  visibility: ${(props) => (props.$showVideo ? 'visible' : 'hidden')};
+  aspect-ratio: 9 / 7.5;
+
+  iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: none;
+  }
+`;
+
+const Transcript = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding-top: 0.5rem;
+  height: 100%;
+  overflow: scroll;
+  visibility: ${(props) => (props.$showVideo ? 'hidden' : 'visible')};
+`;
+
+const DynamicDescription = styled.div`
+  width: 100%;
+  display: ${(props) => (props.$showVideo ? 'block' : 'none')};
+  margin-bottom: 1rem;
+
+  p {
+    font-size: 0.8rem;
+    font-weight: 300;
+    line-height: 1.47;
+    letter-spacing: -0.022px;
+    margin: 0;
+  }
+
+  p b {
+    font-weight: 500;
+  }
+`;
+
 function Pomodoro({ $sidebarState, $projectState }) {
+  const videoDemo = useRef(null);
+  const videoDescription = useRef(null);
+  const transcript = useRef(null);
+  const [showVideo, setShowVideo] = useState(true);
+  const [videoHeight, setVideoHeight] = useState(0);
+  const [transcriptHeight, setTranscripHeight] = useState(0);
+  
+  const updateVideoHeight = () => {
+    const nextVideoHeight = videoDemo.current.clientHeight;
+    if (nextVideoHeight > 1) {
+      setVideoHeight(nextVideoHeight);
+    } else {
+      console.error('Invalid video height @Pomodoro.jsx');
+    }
+
+    if (showVideo) {
+      const nextDescriptionHeight = videoDescription.current.clientHeight;
+      if ((nextDescriptionHeight > 1) && (nextVideoHeight > 1)) {
+        setTranscripHeight(nextDescriptionHeight + nextVideoHeight);
+      } else {
+        console.error('Invalid video and description heights @Pomodoro.jsx');
+      }
+    }
+    
+    // if (videoheight > 1) {
+    //   console.log(`vid height ${videoHeight} and description ${descriptionHeight}`);
+      
+    // } else {
+    //   console.error('Invalid height for video @Pomodoro.jsx');
+    // }
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', updateVideoHeight);
+
+    return () => {
+      window.removeEventListener('resize', updateVideoHeight);
+    }
+  }, [videoHeight, showVideo]);
+
+  useEffect(() => {
+    updateVideoHeight();
+
+    // when click on transcript, reset scroll to top
+    if (!showVideo) {
+      transcript.current.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    }
+
+    console.log('showVideo -- ', showVideo);
+
+  }, [showVideo]);
+
   useEffect(() => {
     const navHeight = 3 * parseFloat(getComputedStyle(document.documentElement).fontSize); // Convert rem to pixels
 
@@ -364,7 +488,7 @@ function Pomodoro({ $sidebarState, $projectState }) {
             alt='Tomato Pomodoro Icon'
           />
         </PomodoroGraphic>
-        <PomodoroSection id="overview-section">
+        <PomodoroSection id='overview-section'>
           <OverviewGrid>
             <OverviewBox>
               <h4>Duration</h4>
@@ -388,7 +512,7 @@ function Pomodoro({ $sidebarState, $projectState }) {
             </OverviewBox>
           </OverviewGrid>
         </PomodoroSection>
-        <PomodoroSection id="problem-section">
+        <PomodoroSection id='problem-section'>
           <h3>Problem</h3>
           <p>
             Engineering students need to complete giant programming assignments without getting 
@@ -397,7 +521,7 @@ function Pomodoro({ $sidebarState, $projectState }) {
             productivity and, potentially, fail to complete assignments.
           </p>
         </PomodoroSection>
-        <PomodoroSection id="background-section">
+        <PomodoroSection id='background-section'>
           <h3>Background</h3>
           <p>
             At UC San Diego, in my Software Engineering course, we were told to create a pomodoro 
@@ -411,7 +535,7 @@ function Pomodoro({ $sidebarState, $projectState }) {
             <li>If a “pomodoro” is interrupted, it is forfeited and must be restarted</li>
           </ul>
         </PomodoroSection>
-        <PomodoroSection id="research-section">
+        <PomodoroSection id='research-section'>
           <h3>Research</h3>
           <h4>Analyze the Competition</h4>
           <p>
@@ -433,53 +557,108 @@ function Pomodoro({ $sidebarState, $projectState }) {
             to analyze how the Technique would work for this audience.
           </p>
         </PomodoroSection>
-        <PomodoroSection id="approach-section">
+        <PomodoroSection id='approach-section'>
           <h3>Approach</h3>
         </PomodoroSection>
-        <PomodoroSection id="design-section">
+        <PomodoroSection id='design-section'>
           <h3>Design</h3>
         </PomodoroSection>
-        <PomodoroSection id="final-section">
+        <PomodoroSection id='final-section'>
           <h3>Final Result</h3>
-          <p>
-            When you open the website for the first time, you'll see that it's set to Work and you can begin
-            a pomodoro when you click Start.
-          </p>
-          <PomodoroGraphic $type={'webpage'}>
-            <img
-              src={Pomo1}
-              alt='Website with a timer saying 25 minutes, a label for Work, and a button with Start'
-            />
-          </PomodoroGraphic>
-          <p>After you've clicked <b>Start</b> and successfully completed one pomo, you'll go into a break.</p>
-          <PomodoroGraphic $type={'webpage'}>
-            <img
-              src={Break1}
-              alt='Website with a timer saying 5 minutes, a label for Short Break, and a button with Start'
-            />
-          </PomodoroGraphic>
-          <p>
-            Once you've finished your break, you can open the settings and modify how long your next pomo
-            should be if you'd prefer to change it.
-          </p>
-          <PomodoroGraphic $type={'webpage'}>
-            <img
-              src={Settings}
-              alt='Settings are open to modify lengths of work sessions and breaks'
-            />
-          </PomodoroGraphic>
-          <p>
-            You can also check out stats which records how many pomos, breaks, and interruptions you have.
-            Even if you leave the window and come back or reload the page, your stats stay the same.
-          </p>
-          <PomodoroGraphic $type={'webpage'}>
-            <img
-              src={Stats}
-              alt='Stats show 1 work session and 1 interruption'
-            />
-          </PomodoroGraphic>
+          <DynamicButtonCont>
+            <DynamicButton selected={showVideo} onClick={() => setShowVideo(true)}>
+              Video
+            </DynamicButton>
+            <DynamicButton selected={!showVideo} onClick={() => setShowVideo(false)}>
+              Transcript
+            </DynamicButton>
+          </DynamicButtonCont>
+          <DynamicVideo id="dynamic-video-container" 
+            $showVideo={showVideo} 
+            style={{ height: showVideo ? videoHeight : transcriptHeight }}>
+            <Video id="video" ref={videoDemo} $showVideo={showVideo}>
+              <iframe
+                title='Pomodoro Demo'
+                src='https://player.vimeo.com/video/909267598?title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=58479'
+                frameBorder='0'
+                allow='autoplay; fullscreen; picture-in-picture'
+              ></iframe>
+              <script src='https://player.vimeo.com/api/player.js' />
+            </Video>
+            <Transcript id="transcript" ref={transcript} $showVideo={showVideo}>
+              <p>
+                When we first open the app, we see a dark screen with six features. From left
+                to right, we have a button for settings, a label indicating a work session, a 
+                timer with 25 minutes, a button to start the timer, a button for information, 
+                and a button for statistics.
+              </p>
+              <p>
+                When we click Start, the timer immediately starts to tick down from 25 minutes.
+                The button that previously said Start has now changed to Restart. Note that we 
+                did speed up this timer for the demo. 
+              </p>
+              <p>
+                At 23 minutes or so, we click Restart to stop the timer and set it back to 25 
+                minutes. This is considered an interruption and we have forfeited this pomodoro.
+              </p>
+              <p>
+                We then open up settings, navigate to the time section and adjust the length of 
+                a work session from 25 to 2. The timer now shows 2 minutes.
+              </p>
+              <p>
+                We click Start and the timer begins to tick down from 2 minutes. While the timer 
+                is running, we click the settings button to open the sidebar and see that all
+                options, except volume, are greyed out and cannot be clicked. No distractions are
+                allowed.
+              </p>
+              <p>
+                When the timer runs out, the label for work turns to short break and we have five
+                minutes on the timer. There are 4 squares below this label that previously were greyed
+                out. One of these squares is now a bright green indicating one successful pomo--or 
+                work session.
+              </p>
+              <p>
+                We open settings again, this time to adjust the length of a short break to 1 minute.
+                Note that settings does not allow you to input less than 1 minute or more than 60
+                minutes because it would go against the pomodoro technique.
+              </p>
+              <p>
+                We click start and let the short break timer run down. Once complete, we click statistics
+                to open up a display of four blocks. From left to right, the first block represents
+                work sessions, then short breaks, then long breaks, and, finally, interruptions. Here, 
+                we see 1 square block indicating a work session, 1 square block for a short break, and 
+                1 red square for an interruption.
+              </p>
+              <p>
+                We open up setting again and navigate to the display section. Here, we click the toggle 
+                button to turn off dark mode. Now, the screen is white and the timer numbers are black.
+                We start the timer to complete our second pomo--or work session.
+              </p>
+              <p>
+                When the timer runs out, we have successfully finished that pomo and we should go into our
+                second short break. We now have 2 green squares on the screen representing our work sessions.
+              </p>
+              <p>
+                We open settings again and navigate to the display section. This time we click the toggle for
+                calm mode, turning it on. Instead of showing minutes and seconds on the timer, the calm mode 
+                shows only minutes. This is supposed to reduce further distractions by displaying fewer changes.
+              </p>
+              <p>
+                With '2m' representing 2 minutes on the timer, we click start and and complete our second break.
+                The video ends after this.
+              </p>
+            </Transcript>
+          </DynamicVideo>
+          <DynamicDescription ref={videoDescription} $showVideo={showVideo}>
+            <p>
+              <b>Demo of the App.</b> See the pomodoro timer in action as we work, take breaks,
+              modify settings, and explore features. For the purposes of the demonstration, we've 
+              sped up the timer. If you'd prefer to try it out yourself, find 
+              it <a href="https://cse110team33.netlify.app">here</a>.
+            </p>
+          </DynamicDescription>
         </PomodoroSection>
-        <PomodoroSection id="insights-section">
+        <PomodoroSection id='insights-section'>
           <h3>Insights</h3>
         </PomodoroSection>
       </PomodoroWrapper>
